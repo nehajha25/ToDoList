d9ae7617441983d6e8420965c0b2109a4ae78810
nehajha25 <nehajha1925@gmail.com>
2024-05-24 15:56:38 +0530

    First local commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default class App extends Component {
+  constructor(props){
+    super(props);
+  
+  this.state = {
+    userName:'Neha',
+    todoItems:[
+      {action:'bird food', done:false},
+      
+    ],
+    newtodo:''
+  };
+  }
+
+  toggleDone=(todo)=>
+    this.setState({
+      todoItems: this.state.todoItems.map((item)=>
+        item.action === todo.action ?{...item,done:!item.done}:item
+
+      ),
+    })
+  
+
+  todorows= ()=>
+    this.state.todoItems.map((item)=>(
+      <tr key={item.action}>
+        <td>{item.action}</td>
+        <td><input type='checkbox' checked={item.done} onChange={()=>this.toggleDone(item)}/></td>
+      </tr>
+
+
+    ));
+
+    updatevalue = (event)=>{
+      this.setState({newtodo:event.target.value});
+    };
+
+    newtodo = ()=>{
+      this.setState({
+        todoItems:[...this.state.todoItems,
+          {action:this.state.newtodo, done:false},
+        ]
+      })
+    }
+  render = ()=>(
+    <div className='container'>   
+      <div className ='row'>
+        <div className ='col-12'>
+         
+          
+          <h2 className ='bg-success text-white text-center'>{this.state.userName} To Do List</h2>
+          <input className='form-control' value={this.state.newtodo} onChange={this.updatevalue}/>
+          <button className='btn btn-primary' onClick={this.newtodo}>Add</button>
+          <div className='col-12'>
+          <table className='table'>
+            <thead>
+              <tr>
+                <th>Task</th>
+                <th>Complete</th>
+              </tr>
+            </thead>
+            <tbody>{this.todorows()}</tbody>
+          </table>
+          </div>
+
+          
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
